79afaaf37d80bc2d6544202820088c447f75c1bc
Stephen Lee <leestephen0320@gmail.com>
2024-09-13 13:56:16 -0700

    added case in page for when a proper character is not selected

--- assets/js/character-bio-script.js
+++ assets/js/character-bio-script.js
@@ -21,33 +21,53 @@ const comboInput = document.querySelector('#combo-container');
 const replaysInput = document.querySelector('#replay-container');
 
 // load page elements based on character object selected
-function loadCharacterName() {
-    nameInput.textContent = ryu.name;
-}
-function loadCharacterImage() {
-    imageInput.src = ryu.profileImg;
-}
-function loadCharacterBio() {
-    bioInput.textContent = ryu.bio;
-}
-function loadCharacterStrategy() {
-    strategyInput.textContent = ryu.strategy;
-}
-function loadCharacterCombo() {
-    comboInput.textContent = ryu.combos;
-}
-function loadCharacterReplay() {
-    replaysInput.textContent = ryu.replays;
+// function loadCharacterName() {
+//     nameInput.textContent = ryu.name;
+// }
+// function loadCharacterImage() {
+//     imageInput.src = ryu.profileImg;
+// }
+// function loadCharacterBio() {
+//     bioInput.textContent = ryu.bio;
+// }
+// function loadCharacterStrategy() {
+//     strategyInput.textContent = ryu.strategy;
+// }
+// function loadCharacterCombo() {
+//     comboInput.textContent = ryu.combos;
+// }
+// function loadCharacterReplay() {
+//     replaysInput.textContent = ryu.replays;
+// }
+function loadPage(charSelect){
+    if (charSelect !== undefined){
+        nameInput.textContent = charSelect.name;
+        imageInput.src = charSelect.profileImg;
+        bioInput.textContent = charSelect.bio;
+        strategyInput.textContent = charSelect.strategy;
+        comboInput.textContent = charSelect.combos;
+        replaysInput.textContent = charSelect.replays;
+    } else {
+        nameInput.textContent = character.name;
+        imageInput.src = character.profileImg;
+        bioInput.textContent = character.bio;
+        strategyInput.textContent = character.strategy;
+        comboInput.textContent = character.combos;
+        replaysInput.textContent = character.replays;
+    }
 }
 
 //render page
 function init() {
-    loadCharacterName();
-    loadCharacterImage();
-    loadCharacterBio();
-    loadCharacterStrategy();
-    loadCharacterCombo;
-    loadCharacterReplay();
+    // loadCharacterName();
+    // loadCharacterImage();
+    // loadCharacterBio();
+    // loadCharacterStrategy();
+    // loadCharacterCombo;
+    // loadCharacterReplay();
+    let charSelect = characters.find(character => character.name === 'fff');
+    console.log(charSelect);
+    loadPage(charSelect);
 }
 
 init();
--- assets/js/character-logic.js
+++ assets/js/character-logic.js
@@ -4,12 +4,12 @@ const characters = [];
 // character object and properties (FAV button should be something saved in localStorage)
 let character = {
     name: 'John Streets',
-    profileImg: '',
-    bio: '',
+    profileImg: 'https://preview.redd.it/by-the-end-of-sf6-john-streetfighter-will-be-the-most-v0-4zl54dv2e1yc1.jpeg?width=1080&crop=smart&auto=webp&s=3d0747840c1170d7e17396d911ae1b71f5dac34a',
+    bio: 'John Streets is the avatar character for World Tour mode. He can learn and use anything from the other characters. If you are on this page, you either have not selected a character or the character is not implemented yet. Please go back and choose a real character.',
     bioShort: '',
-    strategy: '',
-    combos: '',
-    replays: '',
+    strategy: 'Anything anyone can do, he can do too.',
+    combos: 'Any combo from any character and more!',
+    replays: 'some whacky video',
     // a field that should be saved to localStorage
     fav: false,
 };
@@ -35,13 +35,26 @@ function characterFav() {
 }
 
 // creating objects, will need seperate variables to keep the inside of the constructor consice and easier to change
-const ryuProfileImg = 'https://www.streetfighter.com/6/assets/images/character/ryu/ryu.png';
-const ryuBio = 'As the original shoto character, Ryu embodies all the meaningful aspects of Street Fighter gameplay in one concise package. Ryu emphasizes a patient playstyle with strong defensive tools using his signature moves: Hadoken and Shoryuken.';
-const ryuBioShort = 'Ryu controls the flow of battle with his Hadoken and Shoryuken.';
-const ryuStrategy = 'Use different speeds of Hadoken to throw off the their timing. When they try to jump, hit them with the Shoryuken';
-//might need to make combos and replays into an array
-const ryuCombos = 'something to get started';
-const ryuReplays = 'something to watch';
-const ryu = new Character('Ryu',ryuProfileImg,ryuBio,ryuBioShort,ryuStrategy,ryuCombos,ryuReplays);
 
-// 
+    // ryu
+        const ryuProfileImg = 'https://www.streetfighter.com/6/assets/images/character/ryu/ryu.png';
+        const ryuBio = 'As the original shoto character, Ryu embodies all the meaningful aspects of Street Fighter gameplay in one concise package. Ryu emphasizes a patient playstyle with strong defensive tools using his signature moves: Hadoken and Shoryuken.';
+        const ryuBioShort = 'Ryu controls the flow of battle with his Hadoken and Shoryuken.';
+        const ryuStrategy = 'Use different speeds of Hadoken to throw off the their timing. When they try to jump, hit them with the Shoryuken';
+        //might need to make combos and replays into an array
+        const ryuCombos = 'something to get started';
+        const ryuReplays = 'something to watch';
+    const ryu = new Character('Ryu',ryuProfileImg,ryuBio,ryuBioShort,ryuStrategy,ryuCombos,ryuReplays);
+    characters.push(ryu);
+    // luke
+        const lukeProfileImg = 'https://www.streetfighter.com/6/assets/images/character/luke/luke.png';
+        const lukeBio = 'Luke plays similarly to Ryu, but with a stronger emphasis on midrange pressure and whiff punishing.';
+        const lukeBioShort = 'Luke forces the opponent to respect his Sand Blasts and his strong meterless combos.';
+        const lukeStrategy = 'Wear down the opponent with Sand Blasts and forward advancing pokes. When they whiff, punish them heavily';
+        //might need to make combos and replays into an array
+        const lukeCombos = 'something to get started';
+        const lukeReplays = 'something to watch';
+    const luke = new Character('Luke',lukeProfileImg,lukeBio,lukeBioShort,lukeStrategy,lukeCombos,lukeReplays);
+    characters.push(luke);
+ 
+
